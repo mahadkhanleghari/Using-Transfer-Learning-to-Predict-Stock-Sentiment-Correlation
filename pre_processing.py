9a8d38bf2b9bf18c186d9cd0a2373a0f5f5b2dfe
Karam El-loh <karamjoudah@gmail.com>
2019-03-27 16:21:18 -0400

    Added two features
    Added two features, which are:
    1. Using a library called TextBlob, there is a function call that calculates the positivity or negativity of the doc (polarity). The polarity is given as a decimal (from -1 to 1) , where a negative number means that the meaning is negative, and a positive number means that it is positive. The feature is binary, where 1 indicates that the doc object is positive, and 0 indicates that the doc object is negative.
    
    2. The second feature simply checks each word in a doc object. If it exists in the list of positive words, we increment the positive count by 1, and similarly for the negative count. Finally, we take the sum of the positive count and sum of the negative counts, and determine if the doc is negative or positive depending on which count is greater. (i.e. positive count=70, negative_count=50 --> doc objectis positive).
    Feature 2 is also binary ,where it is 1 if the doc is positive, and 0 if the doc is negative

--- pre_processing.py
+++ pre_processing.py
@@ -1,22 +1,34 @@
 import spacy
+from textblob import TextBlob                         #You have to download textblob
 nlp = spacy.load("en_core_web_sm")
+
 
 class Pre_Processing:
 
-    def __init__(self, file_name):
+    def __init__(self, file_name,positive_words_filename,negative_words_filename):
         self.file_name = file_name
+        self.positive_words_filename=positive_words_filename#name of the file that contains the bag of positive words
+        self.negative_words_filename=negative_words_filename#name of the file that contains the bag of negative words
         self.undoced_instance_list = [] #List of all paragraphs in base form. (Not in Spacy Doc form)
         self.doc_instance_list = [] #list of every instance in Spacy doc form
+        self.positive_words_list=[] #list of positive words 
+        self.negative_words_list=[] #list of negative words
+        
 
         """Method Calls Description
         
         1. extract_training calls ___clean_text__.
         2. __clean_text__ returns a refined list undoced_instance_list.
-        3. __spacy_tokenizer__ makes the doc_instance_list."""
+        3. __spacy_tokenizer__ makes the doc_instance_list.
+        4. _get_positive_words places the bag of words in the positive_words.txt file in a list
+        5. _get_negative_words places the bag of words in the negative_words.txt file in a list    """
 
         #Method Calls
         self._extract_training_()
         self._spacy_docizer_()
+        self._get_positive_words_()
+        self._get_negative_words_()
+        
 
 
     def _extract_training_(self):
@@ -40,16 +52,86 @@ class Pre_Processing:
         for instance in self.undoced_instance_list:
             new = nlp(instance)
             self.doc_instance_list.append(new)
+            
+            
+    def _get_positive_words_(self):         #gets a list of positive words from the positive_words.txt file
+        
+        with open(self.positive_words_filename,"r") as pos_words:
+           self.positive_words_list=pos_words.read().split()
+
+         
+    def _get_negative_words_(self):         #gets a list of negative words from the negative_words.txt file
+        
+        with open(self.negative_words_filename,"r") as neg_words:
+            self.negative_words_list=neg_words.read().split()
+        
+   
+        
 
 
 
 
+        
+        
+            
 #main
-file = "train_neg.txt"
-pre = Pre_Processing(file)
+file = "test_neg.txt"
+positive_words_file="positive_words.txt"
+negative_words_file="negative_words.txt"
+pre = Pre_Processing(file,positive_words_file,negative_words_file)
+
 doc_list = pre.doc_instance_list #Important: This is a list of doc type object.
 
-print(doc_list)
+
+
+
+feature1=[]                     #these two lists will contain the features for each doc object
+feature2=[]
+
+negative_words_count=[]         #these two lists are used to calculate the second feature
+positive_words_count=[]
+
+
+for doc in doc_list:
+    doc_blob=TextBlob(doc.text)
+
+    if doc_blob.sentiment.polarity >0.1:       #the textblob object has a function that calculate the positivity or negativity(polarity) of a piece of text
+        feature1.append(1)
+    else:
+        feature1.append(0)
+        
+        
+  #for the second feature, we check each word if it exists in either the positive words list or negative words list
+  #if it exists in the positive words list, we increment the positive_words_count list by 1
+  #if it exists in the positive words list, we increment the negative_words_count list by 1
+   
+    for word in doc:                
+        if word.text in pre.negative_words_list:
+            negative_words_count.append(1)
+        if word.text in pre.positive_words_list:
+            positive_words_count.append(1)
+            
+    #after getting the counts for each word, we calculate the sum of each count. if count of positive >count of negative, then it is positive
+    if sum(positive_words_count)>sum(negative_words_count):
+        feature2.append(1)
+    else:
+        feature2.append(0)
+    positive_words_count.clear()
+    negative_words_count.clear()
+    
+
+
+#note that both features are binary, where 1 indicates that it is positive, and 0 indicates that it is negative
+
+
+            
+            
+     
+        
+
+    
+
+   
 
 
 
